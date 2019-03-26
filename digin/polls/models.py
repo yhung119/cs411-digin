c86f034c9c2283d47de7ee3a7b04eee5a97bd10c
William Xu <williamxu97@yahoo.com>
2019-03-25 20:08:15 -0500

    you can now add questions and choices, userid is attached

--- digin/polls/models.py
+++ digin/polls/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	userID=models.IntegerField(default=0)
 	def __str__(self):
 		return self.question_text
 	def was_published_recently(self):
@@ -17,5 +18,6 @@ class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	choice_text = models.CharField(max_length=200)
 	votes = models.IntegerField(default=0)
+	userID=models.IntegerField(default=0)
 	def __str__(self):
 		return self.choice_text
--- digin/polls/views.py
+++ digin/polls/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
+
+
+from django.forms.formsets import formset_factory
+
+
+
 
 
 class IndexView(generic.ListView):
@@ -26,19 +33,38 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+	print("vote")
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form.
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+		
+def addChoice(request,question_id):
+	print("choice")
+	current_user=request.user
+	print(current_user.id)
+	question = get_object_or_404(Question, pk=question_id)
+	inp_value = request.GET.get('choice')
+	question = get_object_or_404(Question, pk=question_id)
+	question.choice_set.create(choice_text=inp_value,votes=0,userID=request.user.id)
+	return HttpResponseRedirect(reverse('polls:vote', args=(question.id,)))
+	
+def addQuestion(request):
+	print("question")
+	inp_value = request.GET.get('choice')
+	print(request.user.id)
+	q=Question(question_text=inp_value,pub_date=timezone.now(),userID=request.user.id)
+	q.save()
+	return HttpResponseRedirect(reverse('polls:index'))
