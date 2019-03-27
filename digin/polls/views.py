3db07f5d5e721486fdc981a0614bf8e8320c7ab7
jasonwang1019 <b02901019@ntu.edu.tw>
2019-03-27 14:41:47 -0500

    question edit/delete converted to raw sql

--- digin/polls/views.py
+++ digin/polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
@@ -47,8 +48,10 @@ class EditView(generic.DetailView):
     template_name = 'polls/edit.html'
 
 def vote(request, question_id):
-    # question = get_object_or_404(Question, pk=question_id)
-    question = Question.objects.raw("SELECT * FROM polls_question WHERE id = %s", [question_id])[0]
+    try:
+        question = Question.objects.raw("SELECT * FROM polls_question WHERE id = %s", [question_id])[0]
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     choices_query = Choice.objects.raw("SELECT * FROM polls_choice WHERE question_id=%s", [question_id])
     choices = []
     for choice in choices_query:
@@ -56,7 +59,6 @@ def vote(request, question_id):
     
     try:        
         selected_choice = Choice.objects.raw("SELECT * FROM polls_choice WHERE id=%s",[request.POST['choice']])
-
     except (KeyError, Choice.DoesNotExist):
         
         # Redisplay the question voting form.
@@ -79,17 +81,19 @@ def vote(request, question_id):
             return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
         else:
             return render(request, 'polls/detail.html', {
-            'question': question,
-            'choices': choices,
-            'error_message': "You have already voted.",
-        })
-        
+                'question': question,
+                'choices': choices,
+                'error_message': "You have already voted.",
+            })
 
 def addChoice(request, question_id):
     current_user=request.user
     inp_value = request.POST.get('choice')
 
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.raw("SELECT * FROM polls_question WHERE id = %s", [question_id])[0]
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     cursor = connection.cursor()
     cursor.execute("INSERT INTO polls_choice"
                    "(choice_text, votes, owner_id, question_id)"
@@ -100,26 +104,25 @@ def addChoice(request, question_id):
     return HttpResponseRedirect(reverse('polls:detail', args=(question.id,)))
     
 def addQuestion(request):
-    print("question")
     inp_value = request.POST.get('question')
-    print(request.user)
     # q = Question(question_text=inp_value,pub_date=timezone.now(),owner=request.user)
+    # q.save()
     cursor = connection.cursor()
     cursor.execute("INSERT INTO polls_question"
                    "(question_text, pub_date, owner_id)"
                    "VALUES (%s, NOW(), %s)",
                    (inp_value, request.user.id)
                    )
-   
-    # q.save()
     return HttpResponseRedirect(reverse('polls:home'))
 
 def delQuestion(request,question_id):
+    try:
+        question = Question.objects.raw("SELECT * FROM polls_question WHERE id = %s", [question_id])[0]
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     cursor = connection.cursor()
-
     cursor.execute("DELETE FROM polls_question WHERE id=%s AND owner_id=%s",
                     (question_id, request.user.id))
-
     return HttpResponseRedirect(reverse('polls:index'))
     
 #def delChoice(request,choice_id,question_id):
@@ -136,10 +139,11 @@ def delQuestion(request,question_id):
 #</form>
 
 def editQuestion(request,question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.raw("SELECT * FROM polls_question WHERE id = %s", [question_id])[0]
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     inp_value=request.POST.get('choice')
-    print(inp_value)
-    question.question_text=inp_value
-    print(question.question_text)
-    question.save()
+    cursor = connection.cursor()
+    cursor.execute("UPDATE polls_question SET question_text = %s WHERE id=%s", [inp_value, question_id])
     return HttpResponseRedirect(reverse('polls:index'))
